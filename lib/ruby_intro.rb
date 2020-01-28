6dd08c994323320ccc58575ba937bc9143235966
Lida Zhang <lidazhang@CSE-MORTAZA-NB3.local>
2020-01-28 00:33:40 -0600

    hw1 done.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,85 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  end
+  s = 0
+  arr.each{|n| s+=n}
+  return s
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length ==0
+    return arr[0]
+  else
+    return arr.max(2).reduce(:+)
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for i in 0..arr.length-2 do
+    for j in i+1..arr.length-1 do
+      if arr[i]+arr[j] == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  str = 'Hello, '
+  str.concat(name)
+  #puts str
+  return str
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  arr = ['a','e','i','o','u','A','E','I','O','U']
+  return arr.include? s[0]
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  num = s.to_i(2)
+  if num % 4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
-
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    # Instance variables
+    @isbn = isbn
+    @price = price
+
+    raise ArgumentError.new(
+      'Empty ISBN number.'
+    ) if @isbn ==''
+
+    raise ArgumentError.new(
+      'Negative or zero price.'
+    ) if @price <= 0
+  end
+
+  def price_as_string
+    p = @price.to_s.split('.')
+    if p.length == 1
+      return '$'+p[0]+'.00'
+    else
+      if p[1].length == 1
+        return '$'+p[0]+'.'+p[1]+'0'
+      else
+        return '$'+p[0]+'.'+p[1]
+      end
+    end
+  end
+
 end
